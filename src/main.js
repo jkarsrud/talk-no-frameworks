014b24ce9a4f01d2dc72715b83fa700f115905af
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2025-06-03 21:02:28 +0200

    Add styles and plugins

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
+import "./assets/fonts.css";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/black.css";
+import "reveal.js/plugin/highlight/zenburn.css";
+import "./style.css";
 
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import SpeakerNotes from "reveal.js/plugin/notes/notes.esm.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
+import Zoom from "reveal.js/plugin/zoom/zoom.esm.js";
 
-const Deck = new Reveal({ plugins: [Markdown] });
+const Deck = new Reveal({ plugins: [Markdown, SpeakerNotes, Highlight, Zoom] });
 
-Deck.initialize();
+Deck.initialize({
+  controls: false,
+  progress: false,
+  hash: true,
+  width: 1920,
+  height: 1080,
+  margin: 0,
+
+  markdown: {
+    gfm: true,
+  },
+});
